ffb0d7ac7103aedc3fb1cf569d54c02d9d8b8597
Mohamad Al-khatib <alkhatib.m804@gmail.com>
2024-10-27 08:46:36 +0100

    use env variables

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'drizzle-kit'
 
-const env = useRuntimeConfig()
-console.log({ env })
+const { POSTGRES_URI } = useRuntimeConfig()
 
 export default defineConfig({
   dialect: 'postgresql',
   schema: './server/database/schema.ts',
   out: './server/database/migrations',
   dbCredentials: {
-    url: 'postgres://postgres:123321@localhost:5432/nuxt-todo',
+    url: POSTGRES_URI,
   },
 })
